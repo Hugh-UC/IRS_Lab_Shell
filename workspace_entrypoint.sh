bbb365c4cd9e96ced6ec43f138d627dad067a3b6
Hugh-UC <u3276400@uni.canberra.edu.au>
2025-10-06 23:39:18 +1100

    update: Cleanup code comments and runtime messages (echo) in workspace_entrypoint.sh

--- workspace_entrypoint.sh
+++ workspace_entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
+# Set ROS 2 workspace root
 WORKSPACE_ROOT="/root/irslab_ws"
 
-# Source the global ROS environment setup
+# Source the global ROS 2 environment setup
 if [ -f "/opt/ros/$ROS_DISTRO/setup.bash" ]; then
     echo "Sourcing global ROS environment: /opt/ros/$ROS_DISTRO/setup.bash"
     source "/opt/ros/$ROS_DISTRO/setup.bash"
@@ -20,11 +21,6 @@ else
 fi
 
 # Source the local workspace setup, if it exists
-#if [ -f "$WORKSPACE_ROOT/install/setup.bash" ]; then
-#    echo "Sourcing local ROS workspace: $WORKSPACE_ROOT/install/setup.bash"
-#    source "$WORKSPACE_ROOT/install/setup.bash"
-#fi
-
 if [ -f "$WORKSPACE_ROOT/install/local_setup.bash" ]; then
     echo "Sourcing local ROS workspace: $WORKSPACE_ROOT/install/local_setup.bash"
     source "$WORKSPACE_ROOT/install/local_setup.bash"
