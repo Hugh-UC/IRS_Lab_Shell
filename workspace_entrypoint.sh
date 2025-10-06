1b1329b01283334b1e1039ab36b562f6041904ea
Hugh-UC <u3276400@uni.canberra.edu.au>
2025-10-06 23:11:51 +1100

    update: Fixing sourcing local package setup in workspace_entry.sh

--- workspace_entrypoint.sh
+++ workspace_entrypoint.sh
@@ -7,11 +7,16 @@ WORKSPACE_ROOT="/root/irslab_ws"
 if [ -f "/opt/ros/$ROS_DISTRO/setup.bash" ]; then
     echo "Sourcing global ROS environment: /opt/ros/$ROS_DISTRO/setup.bash"
     source "/opt/ros/$ROS_DISTRO/setup.bash"
-else
-	echo "notfound /opt/ros/$ROS_DISTRO/setup.bash"
+
+elif [ -f "/opt/ros/$ROS_DISTRO/install/setup.bash" ]; then
+	echo "notfound: /opt/ros/$ROS_DISTRO/setup.bash"
 
     echo "sourcing /opt/ros/$ROS_DISTRO/install/setup.bash"
     source /opt/ros/$ROS_DISTRO/install/setup.bash
+
+else
+    echo "notfound: /opt/ros/$ROS_DISTRO/install/setup.bash"
+    echo "WARNING: Base ROS environment not sourced!"
 fi
 
 # Source the local workspace setup, if it exists
@@ -20,14 +25,21 @@ fi
 #    source "$WORKSPACE_ROOT/install/setup.bash"
 #fi
 
-if [ -f "$WORKSPACE_ROOT/install/setup.bash" ]; then
-    echo "Sourcing local ROS workspace: $WORKSPACE_ROOT/install/setup.bash"
-    source "$WORKSPACE_ROOT/install/setup.bash"
-elif [ -f "$WORKSPACE_ROOT/install/local_setup.bash" ]; then
+if [ -f "$WORKSPACE_ROOT/install/local_setup.bash" ]; then
     echo "Sourcing local ROS workspace: $WORKSPACE_ROOT/install/local_setup.bash"
     source "$WORKSPACE_ROOT/install/local_setup.bash"
+
+elif [ -f "$WORKSPACE_ROOT/install/setup.bash" ]; then
+    echo "notfound: $WORKSPACE_ROOT/install/local_setup.bash"
+
+    echo "Sourcing local ROS workspace: $WORKSPACE_ROOT/install/setup.bash"
+    source "$WORKSPACE_ROOT/install/setup.bash"
+
 else
-    echo "notfound $WORKSPACE_ROOT/install/setup.bash \n or \n $WORKSPACE_ROOT/install/local_setup.bash"
+    echo "notfound: $WORKSPACE_ROOT/install/setup.bash"
+    echo "WARNING: Local workspace setup files not found in $WORKSPACE_ROOT/install/. Packages not sourced."
 fi
+
+echo "Setup Complete!"
 
 exec "$@"
