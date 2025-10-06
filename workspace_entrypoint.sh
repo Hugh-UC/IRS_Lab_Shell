af695092f7071c2d8e316d88b2a1d2cc5e55ec2a
Hugh-UC <u3276400@uni.canberra.edu.au>
2025-10-06 20:54:08 +1100

    update: Fixing sourcing local package setup in workspace_entry.sh

--- workspace_entrypoint.sh
+++ workspace_entrypoint.sh
@@ -5,12 +5,29 @@ set -e
 if [ -f "/opt/ros/$ROS_DISTRO/setup.bash" ]; then
     echo "Sourcing global ROS environment: /opt/ros/$ROS_DISTRO/setup.bash"
     source "/opt/ros/$ROS_DISTRO/setup.bash"
+else
+	echo "notfound /opt/ros/$ROS_DISTRO/setup.bash"
+
+    echo "sourcing /opt/ros/$ROS_DISTRO/install/setup.bash"
+    source /opt/ros/$ROS_DISTRO/install/setup.bash
 fi
 
 # Source the local workspace setup, if it exists
 if [ -f "$WORKSPACE_ROOT/install/setup.bash" ]; then
     echo "Sourcing local ROS workspace: $WORKSPACE_ROOT/install/setup.bash"
     source "$WORKSPACE_ROOT/install/setup.bash"
+fi
+
+WORKSPACE_ROOT="/root/irslab_ws"
+cd WORKSPACE_ROOT
+colcon build
+
+if [ -f "$WORKSPACE_ROOT/install/setup.bash" ]; then
+    echo "Sourcing local ROS workspace: $WORKSPACE_ROOT/install/setup.bash"
+    source "$WORKSPACE_ROOT/install/setup.bash"
+else
+    echo "Sourcing local ROS workspace: $WORKSPACE_ROOT/install/local_setup.bash"
+    source "$WORKSPACE_ROOT/install/local_setup.bash"
 fi
 
 exec "$@"
